f9ee9ee7d4ca39536e2e1b9a2cf36a20ef6fa1cc
Vladimir Liankevich <liankevichvladimir@gmail.com>
2022-09-21 01:11:51 +0300

    added light and 3 cube with rotations elements

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 
 // Canvas
-
 function main() {
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({
@@ -22,19 +21,42 @@ function main() {
   const boxDepth = 1;
   const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ab55 });
+  function makeInstance(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({ color });
 
-  const cube = new THREE.Mesh(geometry, material);
+    const cube = new THREE.Mesh(geometry, material);
 
-  scene.add(cube);
+    scene.add(cube);
+
+    cube.position.x = x;
+    return cube;
+  }
+
+  const cubes = [
+    makeInstance(geometry, 0x00ab55, 0),
+    makeInstance(geometry, 0x8844aa, -2),
+    makeInstance(geometry, 0xaa8844, 2),
+  ];
+
+  const color = 0xffffff;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
+
+  scene.add(cubes);
   renderer.render(scene, camera);
   console.log('render');
 
   function render(time) {
     time *= 0.001;
 
-    cube.rotation.x = time;
-    cube.rotation.y = time;
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * 0.1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
+    });
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   }
